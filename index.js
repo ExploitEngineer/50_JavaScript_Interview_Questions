15076288d995e3a8beea0567086ad3e53125c4ca
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-28 00:33:20 +0500

    Add 20 questions

---
+++ index.js
@@ -0,0 +1,101 @@
+// 1
+console.log("Abdul Rafay", "Just to do code!");
+
+
+// 2
+console.log(45 * 2 - 10);
+
+
+// 3
+const dt = new Date();
+console.log(dt.getFullYear());
+
+
+// 4
+let first = "Abdul";
+let last = "Rafay";
+console.log(first.concat(" " + last));
+
+
+// 5
+let a = 12;
+console.log(a);
+a = 32;
+console.log(a);
+
+
+// 6
+console.error("something went wrong");
+
+
+// 7
+console.log(12 * 12);
+
+
+// 8
+let b = true;
+console.log(typeof b);
+
+
+// 9
+let myAge = 17;
+if (myAge > 18) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+// 10
+console.log(100 / 0);
+
+
+// 11
+let c = 10;
+console.log(c);
+
+
+// 12
+const PI = Math.PI;
+console.log(PI);
+
+
+// 13
+let d = "dog";
+d = "cat";
+console.log(d);
+
+
+// 14
+console.log(typeof null);
+
+
+// 15
+let strNum = "25";
+console.log(typeof strNum);
+
+
+// 16
+let bool = true;
+console.log(typeof bool);
+
+
+// 17
+let str = "hey", num = 12, bol = true;
+console.log(str, num, bol);
+
+
+// 18
+let e;
+console.log(typeof e);
+
+
+// 19
+let un = undefined;
+console.log(un);
+
+
+// 20
+const arr = [1, 2, 3];
+// arr = [3, 3, 4]; // don't do this with constant variable
+console.log(arr);
