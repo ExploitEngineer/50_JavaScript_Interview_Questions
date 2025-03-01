a7131a3f526cb2e0036831fb5aae335cfb503879
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-02 00:05:24 +0500

    Add 10 Questions

--- index.js
+++ index.js
@@ -99,3 +99,99 @@ console.log(un);
 const arr = [1, 2, 3];
 // arr = [3, 3, 4]; // don't do this with constant variable
 console.log(arr);
+
+
+// 21
+for (let i = 1; i < 51; i++) {
+    console.log(i);
+}
+
+
+// 22
+let i = 1;
+let sum = 0
+while (i < 11) {
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+
+// 23
+let str1 = "JavaScript";
+for (let char of str1) {
+    console.log(char);
+}
+
+
+// 24
+for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) { }
+    else {
+        console.log(i);
+    }
+}
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+
+// 25
+let aa = 5;
+do {
+    console.log(aa);
+    aa--;
+} while (aa > 0);
+
+
+// 26
+let fact = 1;
+for (let i = 5; i > 0; i--) {
+    fact *= i;
+}
+console.log(fact);
+
+
+// 27
+let hold = 1
+for (let i = 1; i < 4; i++) {
+    let str = "";
+    for (let j = 1; j < 4; j++) {
+        str += `${hold} `;
+        hold++;
+    }
+    console.log(str);
+}
+
+
+// 28
+let arr1 = [1, 2, 3, 4, 5];
+for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr1[i];
+    arr1[i] = arr1[arr1.length - 1 - i];
+    arr[arr.length - i - 1] = temp;
+}
+console.log(arr1);
+
+
+// 29
+let num2 = 1;
+while (num2 < 101) {
+    if (num2 % 5 === 0) {
+        console.log(num2);
+    }
+    num2++;
+}
+
+
+// 30
+let obj = {
+    name: "abdul rafay",
+    city: "Islamabad"
+};
+for (let key in obj) {
+    console.log(`${key} and value of obj is ${obj[key]}`);
+}
